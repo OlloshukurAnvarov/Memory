828c8da6ec0802f93ea58d64d88ab8851ee4c3cc
hp <asssas@fgfg>
2023-10-02 13:44:11 +0500

    victory oynasi qo'shildi

--- app/src/main/java/com/leaf/memory/MainActivity.kt
+++ app/src/main/java/com/leaf/memory/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_victory)
 
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) window.attributes.layoutInDisplayCutoutMode =
