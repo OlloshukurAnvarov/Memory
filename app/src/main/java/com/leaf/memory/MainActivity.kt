69fa89fe4bf84a354173a2d17714f3df9ce08b4c
hp <asssas@fgfg>
2023-10-02 14:53:08 +0500

    animals qo'shildi

--- app/src/main/java/com/leaf/memory/MainActivity.kt
+++ app/src/main/java/com/leaf/memory/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_victory)
+        setContentView(R.layout.activity_main)
 
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) window.attributes.layoutInDisplayCutoutMode =
